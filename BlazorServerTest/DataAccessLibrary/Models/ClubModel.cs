736434905d592a882992a1f3b8506a3bb9a116c7
Ismail TURK <ismailturk.10@gmail.com>
2022-05-19 23:58:11 +0300

    Blazor Server Uygulaması
    Blazor server ile MSSQL ' den aldığım verileri arayüzde göstediğim projem

---
+++ BlazorServerTest/DataAccessLibrary/Models/ClubModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAccessLibrary.Models
+{
+    public class ClubModel
+    {
+        public string ClubName { get; set; }
+        public int FoundationDate { get; set; }
+       
+    }
+}
